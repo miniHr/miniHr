3908234f02f32f4e48a1576e789bf37cf68ced26
yinkun <yinkun@allinpay.com>
2017-03-12 21:27:59 +0800

    文职增加下拉框

--- pages/applicant/list/list.js
+++ pages/applicant/list/list.js
@@ -1,21 +1,36 @@
-// pages/applicant/applicant.js
+// pages/applicant/list/list.js
 Page({
   data:{
-    typesOfJobs:[
+    typeOfJobs:[
       {"id":"type1","name":"普工","right":0},
       {"id":"type2","name":"文职","right":0},
       {"id":"type3","name":"销售","right":0},
       {"id":"type4","name":"技工","right":0},
       {"id":"type5","name":"其他","right":0},
-    ]
+    ],
+    typeOfOffices:[
+      {"name":"人事"},
+      {"name":"行政"},
+      {"name":"客服"}
+    ],
+    tab:[true,true,true,true,true]
   },
   reachToDetails:function(e){
     var id=e.currentTarget.id;
+    var tab=this.data.tab;
     if('type2'==id){
-      
+      if(tab[1]==true){
+      this.setData({
+                    tab:[true,false,true,true,true],
+                });
+      }else{
+         this.setData({
+                    tab:[true,true,true,true,true],
+                });
+      }
     }else{
       wx.navigateTo({
-        url: '../applicant/detail/detail'
+        url: '../detail/detail'
       })
     }
   }
