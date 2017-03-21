5023541104ccd3a3b143dca01e9d783cf9a87b78
yinkun <yinkun@allinpay.com>
2017-03-21 22:35:34 +0800

    更新

--- pages/applicant/detail/detail.js
+++ pages/applicant/detail/detail.js
@@ -3,5 +3,15 @@ Page({
   data:{},
   onPullDownRefresh:function(){
     wx.stopPullDownRefresh();
+  },
+  onLoad:function(options){
+    wx.request({
+      url: 'https://localhost:8080/hello',
+      data: {},
+      method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      success: function(res){
+        console.log(res.data);
+      }
+    })
   }
 })
