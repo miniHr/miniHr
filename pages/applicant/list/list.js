7b056fbbbe5dfad11978a2da0ba7276c9ec8f7c7
yinkun <yinkun@allinpay.com>
2017-03-21 20:34:26 +0800

    增加明细页面

--- pages/applicant/list/list.js
+++ pages/applicant/list/list.js
@@ -4,28 +4,31 @@ Page({
     typeOfJobs:[
       {"id":"type1","name":"普工","right":0},
       {"id":"type2","name":"文职","right":0},
+      {"id":"admin","name":"人事","right":0},
+      {"id":"clerk","name":"行政","right":0},
+      {"id":"service","name":"客服","right":0},
       {"id":"type3","name":"销售","right":0},
       {"id":"type4","name":"技工","right":0},
       {"id":"type5","name":"其他","right":0},
     ],
-    typeOfOffices:[
-      {"name":"人事"},
-      {"name":"行政"},
-      {"name":"客服"}
-    ],
-    tab:[true,true,true,true,true]
+    tab:['','',' hide',' hide',' hide','','',''],
+    isImage:['show','show','hide','hide','hide','show','show','show'],
+    rotation:['','','','','','','',''],
+    colors:['#F8F8FF','#F8F8FF','#87CEFA','#5F9EA0','#00FFFF','#F8F8FF','#F8F8FF','#F8F8FF']
   },
   reachToDetails:function(e){
     var id=e.currentTarget.id;
     var tab=this.data.tab;
     if('type2'==id){
-      if(tab[1]==true){
+      if(tab[2]==' hide'){
       this.setData({
-                    tab:[true,false,true,true,true],
+                    tab:['','','','','','','',''],
+                rotation:['','yes','','','','','','']    
                 });
       }else{
          this.setData({
-                    tab:[true,true,true,true,true],
+                    tab:['','',' hide',' hide',' hide','','',''],
+                    rotation:['','','','','','','','']
                 });
       }
     }else{
