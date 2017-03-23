01f9c96eabd2d4cc9ebd5e51b3c26f985a1b7fa8
yinkun <yinkun@allinpay.com>
2017-03-23 22:35:54 +0800

    更新代码

--- pages/applicant/detail/detail.js
+++ pages/applicant/detail/detail.js
@@ -2,9 +2,28 @@
 Page({
   data:{
     hidden:true,
-    jobInfo:[]
+    jobInfo:[],
+    jobType:"1"
   },
   onPullDownRefresh:function(){
+     var that=this;
+    wx.request({
+      url: 'https://561job.cn/'+this.data.jobType,
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function(res){
+        if(200!=res.statusCode){
+          that.setData({
+            hidden:false
+          });
+        }else{
+          that.setData({
+            jobInfo:res.data,
+          })
+        }
+      }
+    })
     wx.stopPullDownRefresh();
   },
   onLoad:function(options){
@@ -20,9 +39,15 @@ Page({
           });
         }else{
           that.setData({
-            jobInfo:res.data
+            jobInfo:res.data,
+            jobType:jobType
           })
         }
+      },
+      fail:function(){
+        that.setData({
+            hidden:false
+          });
       }
     })
   },
