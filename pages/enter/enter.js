82b44ca7b9bbefff051f4f019cfab3ab54d4af9d
yinkun <yinkun@allinpay.com>
2017-06-08 09:27:51 +0800

    禁用个人功能

--- pages/enter/enter.js
+++ pages/enter/enter.js
@@ -4,8 +4,13 @@ Page({
     var that = this;
     var id = e.currentTarget.id;
     if (id == '2') {//个人用户
-      wx.redirectTo({
-        url: '../applicant/applicant?id=' + id
+      // wx.redirectTo({
+      //   url: '../applicant/applicant?id=' + id
+      // })
+      wx.showModal({
+        title: '温馨提示',
+        content: '此功能暂不开放',
+        showCancel:false
       })
     } else {
       wx.redirectTo({//企业用户
