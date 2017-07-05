2155faa528195b3d45f7c36156ef9ee001b784d0
yinkun <yinkun@allinpay.com>
2017-07-05 23:22:52 +0800

    更改button

--- pages/applicant/applicant.js
+++ pages/applicant/applicant.js
@@ -14,109 +14,51 @@ Page({
    */
 
   data: {
-    indexs: 0,
-    hide1: "",
-    hide2: "hide",
-    hide3: "hide",
-    hide4: "hide",
-    sex: "",
-    ages: [],
-    age: 16,
-    industries: ["应届毕业生", "普工", "人事行政", "会计", "服务类", "策划设计", "销售", "技工", "管理", "其他"],
-    industry: "请选择行业",
-    worktimes: [],
-    worktime: "请选择时间",
+    index:0,
+    industries: ["普工", "人事行政", "会计", "服务类", "策划设计", "销售", "技工", "管理", "其他"],
+    industry: "",
     educations: ["小学", "初中", "高中", "中专", "大专", "本科", "硕士"],
-    education: "请选择学历",
-    major: "请填写专业"
+    education: "",
+    ways: ["微信公众号", "QQ群", "智诚招聘网", "论坛门户", "广播电视", "出租车广告", "小程序", "熟人介绍", "其它"],
+    way: "",
+    expectAddres: ["淮北全市", "相山区", "杜集区", "烈山区", "濉溪县", "合肥", "芜湖", "宿州", "徐州/宿迁", "南京/苏州/无锡/常州/镇江", "杭州/宁波/温州", "珠三角"],
+    expectAddr: "",
+    expectType: ""
   },
 
-  //以下为自定义点击事件
-  saveSex: function (e) {//绑定性别
-    var that = this;
-    var id = e.currentTarget.id;
-    var arr = new Array();
-    var arr2 = new Array();
-    for (var i = 0; i <= 44; i++) {
-      arr[i] = i + 16;
-    }
-    for (var i = 0; i <= 50; i++) {
-      arr2[i] = i;
-    }
-    that.setData({
-      sex: id,
-      hide1: "hide",
-      hide2: "",
-      ages: arr,
-      worktimes:arr2
+  bindPickerChange1: function (e) {
+    var arr = this.data.educations;
+    this.setData({
+      education: arr[e.detail.value]
     })
   },
 
-  bindPickerChange1: function (e) {//绑定年龄
-    var arrs = this.data.ages;
+  bindPickerChange2: function (e) {
+    var arr = this.data.industries;
     this.setData({
-      age: arrs[e.detail.value],
-      hide2: "hide",
-      hide3: "",
+      industry: arr[e.detail.value]
     })
   },
-  bindPickerChange2: function (e) {//绑定行业
-    var ins = this.data.industries;
-    var t1 = this.data.worktime;
+
+  bindPickerChange3: function (e) {
+    var arr = this.data.industries;
     this.setData({
-      industry: ins[e.detail.value],
+      expectType: arr[e.detail.value]
     })
-    if (t1 != '请选择时间') {
-      this.setData({
-        hide3: "hide",
-        hide4: "",
-      })
-    }
   },
-  bindPickerChange3: function (e) {//绑定时间
-    var times = this.data.worktimes;
-    var indus = this.data.industry;
+
+  bindPickerChange4: function (e) {
+    var arr = this.data.expectAddres;
     this.setData({
-      worktime: times[e.detail.value]
+      expectAddr: arr[e.detail.value]
     })
-    if (indus != '请选择行业') {
-      this.setData({
-        hide3: "hide",
-        hide4: "",
-      })
-    }
   },
-  bindPickerChange4: function (e) {//绑定学历
-    var edus = this.data.educations;
-    var m = this.data.major;
+
+  bindPickerChange5: function (e) {
+    var arr = this.data.ways;
     this.setData({
-      education: edus[e.detail.value]
+      way: arr[e.detail.value]
     })
-    if (m != '请填写专业') {
-      wx.redirectTo({
-        url: '../job/job?sex=' + this.data.sex + '&age=' + this.data.age + '&industry=' + this.data.industry + '&worktime=' + this.data.worktime + '&education=' + this.data.education + '&major=' + this.data.major
-      })
-    }
-  },
-  bindPickerChange5: function (e) {//绑定专业
-    var str = e.detail.value;
-    var es = this.data.education;
-    if (str == '') {
-      return;
-    }
-    if (es == '请选择学历') {
-      this.setData({
-        major: str
-      })
-      return;
-    } else {
-      this.setData({
-        major: str
-      })
-      wx.redirectTo({
-        url: '../job/job?sex=' + this.data.sex + '&age=' + this.data.age + '&industry=' + this.data.industry + '&worktime=' + this.data.worktime + '&education=' + this.data.education + '&major=' + this.data.major
-      })
-    }
   }
 })
 
